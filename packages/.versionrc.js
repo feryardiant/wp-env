295a2718974924ff80516d0142f0ba8ff7a5c8ea
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-04 15:39:58 +0700

    chore(dev): add script to manage package release versions
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ packages/.versionrc.js
@@ -0,0 +1,13 @@
+import { basename } from "node:path"
+
+export const writerOpts = {
+  transform: (commit, { packageData } = {}) => {
+    const repo = packageData?.repository || {}
+
+    if (!repo.directory || commit?.scope !== basename(repo.directory)) {
+      return false
+    }
+
+    return commit
+  }
+};
