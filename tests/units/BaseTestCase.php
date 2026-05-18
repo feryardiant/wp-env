44148a5280355dda8a72a4b882a8b1283105f4f3
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-18 17:06:37 +0700

    chore: stubs all escape and translation functions
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/units/BaseTestCase.php
+++ tests/units/BaseTestCase.php
@@ -22,14 +22,9 @@ abstract class BaseTestCase extends TestCase
         parent::setUp();
 
         // Mock WP functions used in the main file
-        Functions\when('__')->returnArg(1);
-        Functions\when('_x')->returnArg(1);
-        Functions\when('esc_attr')->returnArg(1);
-        Functions\when('esc_html')->returnArg(1);
-        Functions\when('esc_html__')->returnArg(1);
-        Functions\when('esc_html_e')->echoArg(1);
+        Functions\stubTranslationFunctions();
+        Functions\stubEscapeFunctions();
 
-        Functions\when('is_wp_error')->justReturn(false);
         Functions\when('wp_parse_args')->alias(
             fn($a, $b) => array_merge($b, $a)
         );
